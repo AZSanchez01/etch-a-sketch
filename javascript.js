15a2bcaa0065fda8ee0f3afee40e086d118b2703
aivan <109073612+AZSanchez01@users.noreply.github.com>
2025-06-09 10:40:37 +0800

    Feat: Create and append square divs to container

---
+++ javascript.js
@@ -0,0 +1,16 @@
+
+//Select container
+let container = document.querySelector(".container");
+
+//Get Number and size of each square to fit into container
+let containerSize = 600;
+let sideSquares = 16;
+let totalSquares = sideSquares**2;
+let squareSize = containerSize/sideSquares;
+
+for (let i = 0; i < totalSquares; i++) {
+    let square = document.createElement("div");
+    square.classList.add("square");
+    square.setAttribute("style", `background: green; height: ${squareSize}px; width: ${squareSize}px;`);
+    container.appendChild(square);
+}
