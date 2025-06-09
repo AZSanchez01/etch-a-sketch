c97017ce5f9bbe66bdae7fd108106a2133c3e027
aivan <109073612+AZSanchez01@users.noreply.github.com>
2025-06-09 11:13:33 +0800

    Feat: Add header and adjusted colors of page

--- javascript.js
+++ javascript.js
@@ -4,14 +4,14 @@ let container = document.querySelector(".container");
 
 //Get Number and size of each square to fit into container
 let containerSize = 600;
-let sideSquares = 100;
+let sideSquares = 150;
 let totalSquares = sideSquares**2;
 let squareSize = containerSize/sideSquares;
 
 for (let i = 0; i < totalSquares; i++) {
     let square = document.createElement("div");
     square.classList.add("square");
-    square.setAttribute("style", `background: green; height: ${squareSize}px; width: ${squareSize}px;`);
+    square.setAttribute("style", `background: white; height: ${squareSize}px; width: ${squareSize}px;`);
 
     //Change square color if hovered
     square.addEventListener("mouseover", () => {
