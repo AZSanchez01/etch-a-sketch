7880a29004047de5144d0a3a1028b30795f061fa
aivan <109073612+AZSanchez01@users.noreply.github.com>
2025-06-09 10:54:46 +0800

    Feat: Add function to 'trace'/color created squares

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ let container = document.querySelector(".container");
 
 //Get Number and size of each square to fit into container
 let containerSize = 600;
-let sideSquares = 16;
+let sideSquares = 100;
 let totalSquares = sideSquares**2;
 let squareSize = containerSize/sideSquares;
 
@@ -12,5 +12,11 @@ for (let i = 0; i < totalSquares; i++) {
     let square = document.createElement("div");
     square.classList.add("square");
     square.setAttribute("style", `background: green; height: ${squareSize}px; width: ${squareSize}px;`);
+
+    //Change square color if hovered
+    square.addEventListener("mouseover", () => {
+        square.style.background = "blue";
+    })
+
     container.appendChild(square);
 }
